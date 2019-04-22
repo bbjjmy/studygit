f99d4b744ac1321fe2a12279372ee96c435b0e9b
bbjjmy <2207373981@qq.com>
2019-04-22 10:18:18 +0800

    dierci

--- mayong-common/src/main/java/com/mayong/utils/CollectionUtils.java
+++ mayong-common/src/main/java/com/mayong/utils/CollectionUtils.java
@@ -1,5 +1,42 @@
 package com.mayong.utils;
 
-public class CollectionUtils {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+public class CollectionUtils {
+	//方法1：判断List或Set集合是否有元素，对象为空或对象中没有元素都算没值 (5分)
+	public boolean notEmpty(List<Object> list) {
+		if (list.isEmpty()) {
+			return false;
+		}else {
+			for (Object object : list) {
+				if (object.equals("") || object.equals(" ") ||object==null) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+	//方法2：判断Map集合是否有元素，对象为空或对象中没有元素都算没值 (5分)
+	public boolean notEmpty(Map<Object,Object> map) {
+		if (map.isEmpty()) {
+			return false;
+		}else {
+			Set<Object> keySet = map.keySet();
+			for (Object object : keySet) {
+				if (object.equals("") || object.equals(" ") ||object==null) {
+					return false;
+				}else {
+					Collection<Object> values = map.values();
+					if (object.equals("") || object.equals(" ") ||object==null) {
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}
 }
--- mayong-common/src/main/java/com/mayong/utils/StringUtils.java
+++ mayong-common/src/main/java/com/mayong/utils/StringUtils.java
@@ -1,4 +1,58 @@
 package com.mayong.utils;
 
 public class StringUtils {
+	//方法1：判断源字符串是否有值，空引号(空白字符串)也算没值 (5分)
+	public boolean hasLength(String src) {
+		if (src.isEmpty()) {
+			return false;
+		}else{
+			if (src.equals("") || src.equals(null)) {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+	}
+	//方法2：判断源字符串是否有值，空引号(空白字符串)和空格也算没值 (5分)
+	public boolean hasText(String src) {
+		if (src.isEmpty()) {
+			return false;
+		}else {
+			if (src.equals("") || src==null || src ==" ") {
+				return false;
+			} else {
+				return true;
+			}
+		}
+		
+	}
+	//方法4：判断是否手机 (5分)
+	public boolean isMobile(String src) {
+		String reg1 = "(1)[0-9]{10}";
+		if (src.matches(reg1)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//方法4：判断是否为邮箱 (5分)
+	public boolean isEmail(String src) {
+		String reg1 = "\\w+@\\w+.(com|cn|con|com.cn)";
+		if (src.matches(reg1)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//方法5：反转字符串，例如参数值是“abcdefg”，则输出“gfedcba” (5分)
+	public boolean reverse(String src) {
+		char[] srcc = new char[src.length()];
+		for (int i = 0; i < src.length(); i++) {
+			srcc[src.length()-i-1] = src.charAt(i);
+		}
+		System.out.println(srcc);
+		return false;
+	}
+	
 }
